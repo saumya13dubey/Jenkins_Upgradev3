e9c8cfba940db52e830a8b8a8c0a366e010d5225
saumya13dubey <102130294+saumya13dubey@users.noreply.github.com>
2025-12-06 12:11:44 +0530

    Update Greeter.java
    Added Goodmorning

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Hello team good Morning welcome to the session  !!!");
+	  System.out.println("Hello and Goodmorning from Main Method & Hello team good Morning welcome to the session  !!!");
   }
 	
 }
